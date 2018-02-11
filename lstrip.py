c936ad6fc76bec61513945ac02c4186a4edb7156
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2018-02-11 12:41:19 +0100

    update

--- lstrip.py
+++ lstrip.py
@@ -1,8 +1,8 @@
 '''
-Removes leading whitespace and empty lines from text file
+Removes leading whitespace and empty lines from text file (in-place)
 
 Usage: lstrip.py <filename.txt>
-Overwrites existing file
+Overwrites existing file!
 Useful for cleaning up output from messy log files
 
 netpacket.net
@@ -13,9 +13,15 @@ __author__ = 'Michal Kowalik'
 __version__= '0.2'
 __status__ = 'Prototype'
 
-infile, outputlist = open(sys.argv[1]), []
-for line in infile: outputlist.append(line.lstrip())
-infile.close()
-outfile = open(sys.argv[1], 'w')
-for line in outputlist: outfile.write(line)
-outfile.close()
+def end():
+    print('\nUsage: lstrip.py <filename.txt>')
+    sys.exit()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1: end()
+    infile, outputlist = open(sys.argv[1]), []
+    for line in infile: outputlist.append(line.lstrip())
+    infile.close()
+    outfile = open(sys.argv[1], 'w')
+    for line in outputlist: outfile.write(line)
+    outfile.close()
