363e4665be142dc080d4d64ab903042cca125f30
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2018-05-14 11:37:04 +0200

    Update

--- clientRP.py
+++ clientRP.py
@@ -6,13 +6,15 @@ Uses standard OS calls to collect connection statistics from NIC driver
 Works on Microsoft and Apple platforms
 Use keyboard break to stop
 
+Added ping output.
+
 netpacket.net
 '''
 import subprocess, time, re, os
 
 __author__ = 'Michal Kowalik'
-__credits__= 'Bruce McMurdo'
-__version__= '1.1'
+__credits__= 'Johannes Jobst'
+__version__= '1.0'
 __status__ = 'Prototype'
 
 def apple():
@@ -40,6 +42,11 @@ def microsoft():
     wifi = True
     output = subprocess.check_output('netsh wlan show interfaces').decode('ascii')
     try:
+        ping_output = subprocess.check_output(ping_command).decode('ascii')
+        ping_output = re.search(r'^Reply.+$', ping_output, re.MULTILINE).group(0).split()[-1]
+    except subprocess.CalledProcessError as no_response:
+        ping_output = 'No ICMP data'
+    try:
         ssid = 'SSID:' + re.search(r'SSID.+', output).group(0).split()[-1]
         bssid = 'BSSID:' + re.search(r'BSSID.+', output).group(0).split()[-1]
         channel = 'Ch:' + re.search(r'Channel.+', output).group(0).split()[-1]
@@ -48,6 +55,7 @@ def microsoft():
         signal = re.search(r'Signal.+', output).group(0).split()[-1][:-1]
     except AttributeError:
         print('No data')
+        print(ping_output)
         wifi = False
         pass
     clock = time.asctime().split()[3]
@@ -56,10 +64,22 @@ def microsoft():
         #dbm = 'dBm:' + str(int(signal) / 2 - 100)
         dbm = signal + '%'
         print(clock, ssid, bssid, dbm, channel, txrate, rxrate)
+        #print(ping_output)
     wifi = True
     time.sleep(0.5)
 
+def ping():
+    """Collects IP of host for ICMP test, returns formatted Ping command
+    """
+    ping_timeout_ms = '200'
+    ping_host = input('\nEnter IP to test [192.0.2.1]: ')
+    if ping_host == '': ping_host = '127.0.0.1'
+    #if operating_system == 'posix': ping_cmd = ('ping -w ' + ping_timeout_ms + ' -n 1 ' + ping_host)
+    if operating_system == 'nt': ping_cmd = ('ping -w ' + ping_timeout_ms + ' -n 1 ' + ping_host)
+    return(ping_cmd)
+
 operating_system = os.name
+ping_command = ping()
 try:
     while True:
         if operating_system == 'posix': apple()
