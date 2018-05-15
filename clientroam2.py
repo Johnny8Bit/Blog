a10dcb5191cf23b6a7165c67a74e68b837de2b75
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2018-05-15 10:42:14 +0200

    Update

--- clientroam2.py
+++ clientroam2.py
@@ -6,7 +6,7 @@ Uses standard OS calls to collect connection statistics from NIC driver
 Works on Microsoft and Apple platforms
 Use keyboard break to stop
 
-Added ping output.
+Added ICMP check for Microsoft OS.
 
 netpacket.net
 '''
@@ -18,8 +18,9 @@ __version__= '1.0'
 __status__ = 'Prototype'
 
 def apple():
-    """Outputs data for Apple device
+    """Outputs data for Apple OS
     """
+    wifi = True
     command = 'exec /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I'
     output = subprocess.check_output(command,shell=True).decode('ascii')
     try:
@@ -30,21 +31,22 @@ def apple():
         signal = 'Signal:' + re.search(r'CtlRSSI.+', output).group(0).split()[-1]
         noise = 'Noise' + re.search(r'agrCtlNoise.+', output).group(0).split()[-1]
     except AttributeError:
-        print('No data')
-        pass
-    clock = time.asctime().split()[3]
-    print(clock, ssid, bssid, channel, txrate, noise, signal)
-    time.sleep(0.5)
+        wifi = False
+        print('No RF data')
+    if wifi:
+        clock = time.asctime().split()[3]
+        print(clock, ssid, bssid, channel, txrate, noise, signal)
+    time.sleep(0.3)
 
 def microsoft():
-    """Outputs data for Microsoft device
+    """Outputs data for Microsoft OS
     """
     wifi = True
     output = subprocess.check_output('netsh wlan show interfaces').decode('ascii')
     try:
         ping_output = subprocess.check_output(ping_command).decode('ascii')
-        ping_output = re.search(r'^Reply.+$', ping_output, re.MULTILINE).group(0).split()[-1]
-    except subprocess.CalledProcessError as no_response:
+        ping_output = re.search(r'^Reply.+$', ping_output, re.MULTILINE).group(0)
+    except subprocess.CalledProcessError:
         ping_output = 'No ICMP data'
     try:
         ssid = 'SSID:' + re.search(r'SSID.+', output).group(0).split()[-1]
@@ -54,35 +56,33 @@ def microsoft():
         rxrate = 'RX:' + re.search(r'Receive.+', output).group(0).split()[-1]
         signal = re.search(r'Signal.+', output).group(0).split()[-1][:-1]
     except AttributeError:
-        print('No data')
-        print(ping_output)
         wifi = False
-        pass
-    clock = time.asctime().split()[3]
+        print('No RF data')
     if wifi:
-        pass
-        #dbm = 'dBm:' + str(int(signal) / 2 - 100)
+        clock = time.asctime().split()[3]
         dbm = signal + '%'
+        #dbm = 'dBm:' + str(int(signal) / 2 - 100)
         print(clock, ssid, bssid, dbm, channel, txrate, rxrate)
-        #print(ping_output)
-    wifi = True
-    time.sleep(0.5)
+        print(ping_output)
+    time.sleep(0.3)
 
 def ping():
     """Collects IP of host for ICMP test, returns formatted Ping command
     """
-    ping_timeout_ms = '200'
+    ping_timeout_ms = '300'
+
     ping_host = input('\nEnter IP to test [192.0.2.1]: ')
-    if ping_host == '': ping_host = '127.0.0.1'
-    #if operating_system == 'posix': ping_cmd = ('ping -w ' + ping_timeout_ms + ' -n 1 ' + ping_host)
+    if ping_host == '': ping_host = '192.0.2.1'
+    if operating_system == 'posix': ping_cmd = ('ping -t ' + ping_timeout_ms + ' -c 1 ' + ping_host)
     if operating_system == 'nt': ping_cmd = ('ping -w ' + ping_timeout_ms + ' -n 1 ' + ping_host)
     return(ping_cmd)
 
-operating_system = os.name
-ping_command = ping()
-try:
-    while True:
-        if operating_system == 'posix': apple()
-        if operating_system == 'nt': microsoft()
-except KeyboardInterrupt:
-    print('Script stopped.')
+if __name__ == '__main__':
+    operating_system = os.name
+    ping_command = ping()
+    try:
+        while True:
+            if operating_system == 'posix': apple()
+            if operating_system == 'nt': microsoft()
+    except KeyboardInterrupt:
+        print('Script stopped.')
